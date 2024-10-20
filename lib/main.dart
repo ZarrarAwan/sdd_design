27139d4ab11da1b5dd9084997bcdb14de4f62cfe
ZarrarAwan <zarrar67@gmail.com>
2024-10-20 20:38:10 +0500

    zarrar
    zarrar changes:
    commenting and font style commenting.

--- lib/main.dart
+++ lib/main.dart
@@ -35,8 +35,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ProfileScreen(),
-      // home: CommunitySupportScreen(),
+      //home: ProfileScreen(),
+       home: CommunitySupportScreen(),
     );
   }
 }
